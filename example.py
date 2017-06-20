d3c18c69d5ce9c2b4b3eec58a554834dd98c9393
k <rockkoca@gmail.com>
2017-06-20 10:36:14 -0700

    test

--- example.py
+++ example.py
@@ -292,9 +292,9 @@ def get_level_1_quotes_and_trades(ticker: str, seconds: int):
         # quote_conn.watch('NVDA')
         # quote_conn.regional_watch(ticker)
         # quote_conn.regional_watch('NVDA')
-        print(update_mongo.get_symbols())
+        # print(update_mongo.get_symbols())
         for symbol in update_mongo.get_symbols():
-            print(symbol, end=', ')
+            print(symbol)
             quote_conn.watch(symbol)
             # quote_conn.regional_watch(symbol)
 
