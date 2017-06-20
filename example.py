ea48b68e207ff2e8559e71d041b6194f2ad50b89
k <rockkoca@gmail.com>
2017-06-20 10:44:25 -0700

    test

--- example.py
+++ example.py
@@ -203,7 +203,7 @@ class MyQuoteListener(iq.SilentQuoteListener):
 
     def process_summary(self, summary: np.array) -> None:
         if is_server():
-            if summary[64] != self.summary_tick_id:
+            if len(summary) > 64 and summary[64] != self.summary_tick_id:
                 self.update_mongo.update_quote(summary)
                 self.summary_tick_id = summary[64]
 
