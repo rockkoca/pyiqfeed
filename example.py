d13a6f2fb922165a63a1e365aae33162488b4692
k <rockkoca@gmail.com>
2017-06-27 09:20:56 -0700

    quick update

--- example.py
+++ example.py
@@ -46,14 +46,14 @@ if __name__ == "__main__":
     # set_interval(check_connection, 5)
 
     # wait 10 till the service is started
-    threading.Timer(10, check_connection).start()
+    threading.Timer(15, check_connection).start()
 
     pool = {}
     #
     bars = threading.Timer(1, get_live_multi_interval_bars, [{}, 30, 1, True])
     bars.start()
 
-    lv1 = threading.Timer(1, get_level_1_multi_quotes_and_trades, [{}, 1, True])
+    lv1 = threading.Timer(5, get_level_1_multi_quotes_and_trades, [{}, 1, True])
     lv1.start()
 
     while 1:
