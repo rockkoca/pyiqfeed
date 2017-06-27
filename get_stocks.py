a3df9af2fc256a1dadff38fd21ccf404b339e631
k <rockkoca@gmail.com>
2017-06-27 10:53:32 -0700

    quick update

--- get_stocks.py
+++ get_stocks.py
@@ -21,7 +21,7 @@ def get_sp500():
         symbols = bs.find_all('input', {'name': 'symbol'})
         # print(symbols)
         stocks = {}
-        for symbol in symbols[:40]:
+        for symbol in symbols[:10]:
             # print(symbol.attrs['value'])
             stocks[symbol.attrs['value']] = {
                 'auto': {
