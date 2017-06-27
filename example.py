e5e9fef11712e89b94da687a68038d6c3c723e9d
k <rockkoca@gmail.com>
2017-06-27 09:18:36 -0700

    quick update

--- example.py
+++ example.py
@@ -43,7 +43,11 @@ if __name__ == "__main__":
 
     # We can use a with statement to ensure threads are cleaned up promptly
     # TODO Fix MongoClient opened before fork. Create MongoClient
-    set_interval(check_connection, 5)
+    # set_interval(check_connection, 5)
+
+    # wait 10 till the service is started
+    threading.Timer(10, check_connection, [3]).start()
+
     pool = {}
     #
     bars = threading.Timer(1, get_live_multi_interval_bars, [{}, 30, 1, True])
