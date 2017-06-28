2b249bd7f71182ccd1e0a6edf0f436e9f18c6406
k <rockkoca@gmail.com>
2017-06-28 09:36:55 -0700

    quick update detecting the rebound stock
    TEST AMD, XIC, NVDA

--- get_stocks.py
+++ get_stocks.py
@@ -21,7 +21,7 @@ def get_sp500():
         symbols = bs.find_all('input', {'name': 'symbol'})
         # print(symbols)
         stocks = {}
-        for symbol in symbols[:10]:
+        for symbol in ['AMD', 'XIV', 'NVDA']:
             # print(symbol.attrs['value'])
             stocks[symbol.attrs['value']] = {
                 'auto': {
